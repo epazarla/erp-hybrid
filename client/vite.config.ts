0d6c4fff9b139b7c7278b36003bd4a957a51f881
epazarla <epazarla@gmail.com>
2025-06-05 14:13:29 +0300

    Fix: Netlify yapılandırmasını düzelt ve base URL'i güncelle

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/erp/',
+  base: '/', // Base URL'i kök dizin yapıyoruz
   plugins: [react()],
   define: {
     'process.env': {},
